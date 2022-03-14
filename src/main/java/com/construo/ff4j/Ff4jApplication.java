39378aede4d1ba2dbcfa14eb5b7bce08f2f8af3f
Michael Robles <mroblesferreira@gmail.com>
2022-03-14 19:31:31 +0000

    Initial commit

---
+++ src/main/java/com/construo/ff4j/Ff4jApplication.java
@@ -0,0 +1,13 @@
+package com.construo.ff4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ff4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ff4jApplication.class, args);
+	}
+
+}
